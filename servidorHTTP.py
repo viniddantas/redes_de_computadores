14c98d629aa01cd0d9d97e458ecdbd3da329f3ab
vinicius dantas <vini.dantas61@gmail.com>
2026-03-24 11:51:08 -0300

    first commit

---
+++ servidorHTTP.py
@@ -0,0 +1,97 @@
+#implementação de um servidor base para interpratação de métodos HTTP
+
+import socket
+
+# Nosso banco de dados simulado
+banco_de_carros = {
+    "ferrari": {
+        "nome": "Ferrari 458 ITALIA",
+        "imagem": "/img/ferrari.jpg "
+    },
+    "pors": {
+        "nome": "Honda Civic",
+        "preco": "R$ 120.000",
+        "imagem": "/img/civic.jpg"
+    },
+    "celta": {
+        "nome": "Chevrolet Celta",
+        "preco": "R$ 18.000",
+        "imagem": "/img/celta.jpg"
+    }
+}
+
+
+
+#definindo o endereço IP do host
+SERVER_HOST = ""
+#definindo o número da porta em que o servidor irá escutar pelas requisições HTTP
+SERVER_PORT = 8080
+
+#vamos criar o socket
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#vamos setar a opção de reutilizar sockets já abertos
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+#atrela o socket ao endereço da máquina e ao número de porta definido
+server_socket.bind((SERVER_HOST, SERVER_PORT))
+
+#coloca o socket para escutar por conexões
+server_socket.listen(1)
+
+#mensagem inicial do servidor
+print("Servidor em execução...")
+print("Escutando por conexões na porta %s" % SERVER_PORT)
+
+#cria o while que irá receber as conexões
+while True:
+    #espera por conexões
+    #client_connection: o socket que será criado para trocar dados com o cliente de forma dedicada
+    #client_address: tupla (IP do cliente, Porta do cliente)
+    client_connection, client_address = server_socket.accept()
+
+    #pega a solicitação do cliente
+    request = client_connection.recv(1024).decode('utf-8')
+    #verifica se a request possui algum conteúdo (pois alguns navegadores ficam periodicamente enviando alguma string vazia)
+    if request:
+        #imprime a solicitação do cliente
+        # print("INICIO Request:", request, "FIM REQUEST")
+
+        #analisa a solicitação HTTP
+        headers = request.split("\n")
+        method = headers[0].split()[0]
+        print(method)
+        if method == 'GET':
+            # pega o nome do arquivo sendo solicitado
+            filename = headers[0].split()[1]
+            print("\nFilename = ", filename, "\n")
+            #verifica qual arquivo está sendo solicitado e envia a resposta para o cliente
+            if filename == "/":
+                filename = "/index.html"
+
+            #try e except para tratamento de erro quando um arquivo solicitado não existir
+            try:
+                print("htdocs" + filename)
+                #abrir o arquivo e enviar para o cliente
+                fin = open("htdocs" + filename)
+                #leio o conteúdo do arquivo para uma variável
+                content = fin.read()
+                #fecho o arquivo
+                fin.close()
+                #envia a resposta
+                response = "HTTP/1.1 200 OK\n\n" + content
+            except FileNotFoundError:
+                #caso o arquivo solicitado não exista no servidor, gera uma resposta de erro
+                response = "HTTP/1.1 404 NOT FOUND\n\n<h1>ERROR 404!<br>File Not Found!</h1>"
+            #envia a resposta HTTP
+            client_connection.sendall(response.encode())
+        elif method == 'POST':
+            try:
+                dados = headers[0].split()[1]
+                print(dados)
+            except FileNotFoundError:
+                response = "HTTP/1.1 404 NOT FOUND\n\n<h1>ERROR 404!<br>File Not Found!</h1>"
+        else:
+            client_connection.close()
+
+server_socket.close()
